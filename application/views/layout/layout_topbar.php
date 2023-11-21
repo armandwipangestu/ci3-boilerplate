31dc0f9190a213fe0148b8196987d570e5aed8b2
Arman <armandwi.pangestu7@gmail.com>
2023-11-21 15:29:29 +0700

    update: add change password dropdown on topbar

--- application/views/layout/layout_topbar.php
+++ application/views/layout/layout_topbar.php
@@ -1,41 +1,3 @@
-    <!-- <div id="main" style="min-height: 837px;">
-        <header class="mb-3">
-            <a href="#" class="burger-btn d-block d-xl-none">
-                <i class="bi bi-justify fs-3"></i>
-            </a>
-        </header>
-
-        <div class="page-heading">
-            <div class="page-title mb-5">
-                <div class="float-start">
-                    <h3><?= $title; ?></h3>
-                </div>
-                <div class="float-end">
-                    <nav>
-                        <ul class="navbar-nav navbar-right">
-                            <li class="dropdown me-1 mb-1">
-                                <a href="#" class="dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
-                                    <img alt="image" src="<?= base_url(); ?>assets/img/avatar_image/<?= $user['avatar_image'] ?>" class="rounded-circle mr-1" style="width: 30px;">
-                                    <div class="d-none d-md-inline-block">Hi, <?= $user['username']; ?></div>
-                                </a>
-                                <div class="dropdown-menu">
-                                    <a href="<?= base_url('user'); ?>" class="dropdown-item has-icon">
-                                        <i class="far fa-user"></i> My Profile
-                                    </a>
-                                    <a href="#" class="dropdown-item has-icon">
-                                        <i class="fas fa-user-edit"></i> Settings
-                                    </a>
-                                    <a href="#" data-url="<?= base_url('auth/logout'); ?>" class="dropdown-item has-icon text-danger" id="logout">
-                                        <i class="fas fa-sign-out-alt"></i> Logout
-                                    </a>
-                                </div>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div> -->
-
     <div id="main" class="layout-navbar navbar-fixed">
         <!-- layout-navbar navbar-fixed or min-vh-100 -->
         <header>
@@ -70,8 +32,15 @@
                                     <h6 class="dropdown-header">Hello, <?= $user['username']; ?>!</h6>
                                 </li>
                                 <li>
-                                    <a class="dropdown-item" href="<?= base_url('user'); ?>"><i class="icon-mid bi bi-person me-2"></i> My
-                                        Profile
+                                    <a class="dropdown-item" href="<?= base_url('user'); ?>">
+                                        <i class="icon-mid bi bi-person me-2"></i>
+                                        My Profile
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="<?= base_url('user/change_password'); ?>">
+                                        <i class="icon-mid bi bi-key me-2"></i>
+                                        Change Password
                                     </a>
                                 </li>
                                 <li>
