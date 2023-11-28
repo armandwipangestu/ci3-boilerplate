0e790d2c1f771af56fe40f0b149e9c1289999b25
Arman <armandwi.pangestu7@gmail.com>
2023-11-28 19:54:44 +0700

    feat: add log mvc

--- application/models/LogAction_model.php
+++ application/models/LogAction_model.php
@@ -7,4 +7,21 @@ class LogAction_model extends CI_Model
     {
         $this->db->insert('user_log_action', $data);
     }
+
+    public function getAllLog()
+    {
+        $query = "SELECT
+                ula.id, first_name, last_name, 
+                username, avatar_image,
+                role, action, 
+                ula.created_at, ula.updated_at
+            FROM user_data AS ud
+            JOIN user_role AS ur
+                ON ud.role_id = ur.id
+            JOIN user_log_action AS ula
+                ON ud.id = ula.user_id
+        ";
+
+        return $this->db->query($query)->result_array();
+    }
 }
